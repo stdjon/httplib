df80b600ca7dd02b57b1f7e7100f63e79118c92f
jon <stdjon@users.noreply.github.com>
2015-06-11 20:40:22 -0700

    [http]
    Add Transform column to Users table.
    Add "Default post format" selector to user prefs page.
    Make "Post format" selector on newthread page respect user's preference.

--- src/forum/www/prefs.js
+++ src/forum/www/prefs.js
@@ -3,14 +3,16 @@ var prf = {
     _loc: '',
     id: '',
     col_id: '',
+    trans: '',
 };
 
 
-function setPrefs(i, m, l, c) {
+function setPrefs(i, m, l, c, t) {
     prf.id = i;
     _g.Motto = prf.motto = m;
     _g.Location = prf._loc = l;
     _g.ColourId = prf.col_id = c;
+    _g.Transform = prf.trans = t;
     $('span#' + c).addClass('selected');
 }
 
@@ -18,12 +20,20 @@ function setPrefs(i, m, l, c) {
 function onKeypress() {
     prf.motto = $('#motto').val();
     prf._loc = $('#_loc').val();
+    prf.trans = $('#transform label.active input').val();
     var ok1 = (prf.motto !== _g.Motto);
     var ok2 = (prf._loc !== _g.Location);
     var ok3 = (prf.col_id !== _g.ColourId);
-    var canupdate = ok1 || ok2 || ok3;
+    var ok4 = (prf.trans !== _g.Transform);
+    var canupdate = ok1 || ok2 || ok3 || ok4;
     $('#update').attr('disabled', canupdate ? null : 'disabled');
     $('#notify').html('');
+}
+
+
+function onRadio() {
+    // this is a bit of a cheat, but it'll do for now...
+    setTimeout(onKeypress, 250);
 }
 
 
@@ -36,7 +46,8 @@ function update() {
         data: 'i=' + prf.id +
             '&m=' + encodeURIComponent(prf.motto) +
             '&l=' + encodeURIComponent(prf._loc) + 
-            '&c=' + encodeURIComponent(prf.col_id),
+            '&c=' + prf.col_id +
+            '&r=' + prf.trans,
         success: function() {
             $('#update').attr('disabled', 'disabled');
             $('#notify').html('Preferences updated!');
