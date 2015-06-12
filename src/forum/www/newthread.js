64df1418c90e8ea3d4cf93117f5b677ece2fa724
jon <stdjon@users.noreply.github.com>
2015-06-11 19:50:02 -0700

    [http]
    Adding rendering and post preview to the newthread page.
    Adding own-post and own-thread CSS classes to thread page.
    Print threads and posts content using triple-brackets (use HTML markup)

--- src/forum/www/newthread.js
+++ src/forum/www/newthread.js
@@ -4,7 +4,7 @@ $(function() {
         var area = $('textarea', $(this));
         var span = $('span', $(this));
         area.bind('input', function() {
-        span.text(area.val());
+            span.text(area.val());
         });
         span.text(area.val());
         $(this).addClass('active');
@@ -29,3 +29,21 @@ function firstPost() {
         $('#fp-controls').addClass('concealed');
     }
 }
+
+
+function showPreview() {
+    var txt = $('#fp-area textarea').val();
+    var sel = $('#fp-render label.active input').val();
+    $.ajax({
+        type: 'POST',
+        dataType: 'text',
+        global: false,
+        url: '/preview-post',
+        data: 'r=' + sel + '&t=' + encodeURIComponent(txt),
+        success: function(data) {
+            $('#preview').html(data);
+        }
+    });
+}
+
+
