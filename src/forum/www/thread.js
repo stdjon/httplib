956d7de420dcc5806df3f1d8f9df1e11b9aa8f5b
jon <stdjon@users.noreply.github.com>
2015-08-20 00:04:40 -0700

    [http]
    Editable thread title (on thread page) for the thread owner.

--- src/forum/www/thread.js
+++ src/forum/www/thread.js
@@ -539,6 +539,79 @@ function replyFirst() {
 }
 
 
+var _isEdit = false;
+var _isEditCancel = false;
+function editTitle() {
+
+    function resetState() {
+        if(!_isEditCancel) {
+            var title = $('#title-text').val();
+            $('#title').text(title);
+            document.title = title;
+        }
+        $('#title').css('display', 'inline');
+        $('#title-edit').css('display', 'none');
+        $('#edit').removeClass('fa-check').addClass('fa-edit');
+        $('#edit').attr('title', 'Update thread title');
+        $('#title-text').keydown(undefined);
+    }
+
+    if(_isEdit) {
+        var title = $('#title-text').val();
+        //alert('New title: "' + title + '"');
+        if(_isEditCancel) {
+            resetState();
+            _isEditCancel = false;
+            _isEdit = false;
+        } else {
+            $.ajax({
+                type: 'POST',
+                dataType: 'text',
+                global: false,
+                url: '/update-title',
+                data: 'th=' + _g.ThreadId + '&t=' + encodeURIComponent(title),
+                success: resetState,
+                error: function() {
+                    alert('error updating title :(');
+                },
+                complete: function() {
+                    _isEdit = false;
+                }
+            });
+        }
+    } else {
+        var title = $('#title').text();
+        $('#title').css('display', 'none');
+        $('#title-edit').css('display', 'block');
+        $('#title-text').text(title).focus();
+        $('#edit').removeClass('fa-edit').addClass('fa-check');
+        $('#edit').attr('title', 'Update thread title');
+        initalizeExpandingAreas();
+        $('#title-text').keydown(titleKeyHandler);
+
+        _isEdit = true;
+    }
+}
+
+
+function titleKeyHandler(e) {
+    if(e.ctrlKey) {
+        switch(e.keyCode) {
+            case 13: { //'Enter'
+                editTitle();
+                e.preventDefault();
+                break;
+            }
+        }
+    }
+    if(e.keyCode === 27) {
+        _isEditCancel = true;
+        editTitle();
+        e.preventDefault();
+    }
+}
+
+
 // install handler for (hacked) keypress events from select2 elements
 $(document).ready(function() {
     $('body').on("s2_keypress", function(_, e) {
