18b60f7915355aeb4142fe7d4244c11a7a08cd86
jon <stdjon@users.noreply.github.com>
2015-09-17 22:01:24 -0700

    [http]
    Specify a minimum number of lines which an expanding-area can occupy in the <div>
    markup (data-size="n", default is 1). The user input text is padded with newlines
    to fit the size.

--- src/forum/www/expanding-area.js
+++ src/forum/www/expanding-area.js
@@ -2,13 +2,23 @@
 // Initialize expanding-areas
 function initalizeExpandingAreas() {
     $(function() {
+        function pad(s, n) {
+            var l = s.split('\n').length - 1;
+            var p = n - l;
+            if(p > 0) {
+                s += (new Array(p).join('\n'));
+            }
+            return s;
+        }
+
         $('div.expanding-area').each(function() {
+            var size = $(this).data('size') || 1;
             var area = $('textarea', $(this));
             var span = $('span', $(this));
             area.bind('input', function() {
-                span.text(area.val());
+                span.text(pad(area.val(), size));
             });
-            span.text(area.val());
+            span.text(pad(area.val(), size));
             $(this).addClass('active');
         });
     });
